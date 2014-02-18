a1022ae78d797726512ecd34e2c3e2633a1639db
alu0100600582 <alu0100600582@ull.edu.es>
2014-02-18 10:56:14 +0000

    Inicializando repositorio prac2 Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prac2::Application.config.secret_key_base = 'd43e4ea6d5df9c6b76b66879dcb3b91c0a05ab16bd7bc1f01cb8290032de1a7da9a7abdeb14c3b859c611d8fa5efed161ece90ada075a907d425f25e0c9a2e70'
